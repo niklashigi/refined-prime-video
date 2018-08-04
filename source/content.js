1456167e905e3421ee2aebe2ae7aa8352b4a3f86
Niklas Higi <niklas@shroudedcode.com>
2018-08-04 21:15:08 +0200

    Only improve "Prime Video" navigation bar for signed-in users
    I mistakenly thought that the design of the navigation bar differed
    between sites, so I created seperate styles for the two variants I
    encountered. Now, I realized that the style only depends on whether
    the user is signed in, so I removed the styles for the second (not
    signed in) variant.

--- source/content.js
+++ source/content.js
@@ -1,7 +1,4 @@
-const navA = !!document.querySelector('.av-retail-m-nav-container')
-const navB = !!document.querySelector('#nav-subnav[data-category="instant-video"]')
-
-const bodyClasses = document.body.classList
-bodyClasses.toggle('rpv-inav', navA || navB)
-bodyClasses.toggle('rpv-inav-a', navA)
-bodyClasses.toggle('rpv-inav-b', navB)
+document.body.classList.toggle(
+  'rpv-inav',
+  document.querySelector('.av-retail-m-nav-container'),
+)
