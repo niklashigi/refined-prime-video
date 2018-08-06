8c41a6c3b8b2b6a24721562737944174cae3c64b
Niklas Higi <niklas@shroudedcode.com>
2018-08-06 16:36:38 +0200

    Hide spoilers (thumbnails and descriptions of unwatched episodes)

--- source/content.js
+++ source/content.js
@@ -1,10 +1,14 @@
 import elementReady from 'element-ready'
 
 import improveNav from './features/improve-nav'
+import hideSpoilers from './features/hide-spoilers'
 
 elementReady('.av-retail-m-nav-container').then(() => {
   console.groupCollapsed('Refined Prime Video is being set up ...')
+
   improveNav()
+  hideSpoilers()
+
   console.groupEnd()
   console.log('Refined Prime Video has been set up!')
 })
