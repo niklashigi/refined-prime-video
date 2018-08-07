5338b3f0c7c528eda3c384ab8be64eab42705e9d
Niklas Higi <niklas@shroudedcode.com>
2018-08-07 23:15:11 +0200

    Add German locales

---
+++ source/locales/de.js
@@ -0,0 +1,12 @@
+module.exports = {
+  settings: {
+    showSpoilers: {
+      options: {
+        never: 'Spoiler nie anzeigen',
+        onHover: 'Spoiler beim Drüberfahren anzeigen',
+        always: 'Spoiler immer anzeigen',
+      },
+      title: 'Diese Einstellung bestimmt wann das Vorschaubild und die Beschreibung von noch nicht angesehen Episoden angezeigt werden.',
+    },
+  },
+}
