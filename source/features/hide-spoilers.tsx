a956afeb4ef3d6906d8a2fd2efdbaa74d6a46d50
Niklas Higi <niklas@shroudedcode.com>
2018-08-13 14:11:55 +0200

    Update spoiler state after an episode has been watched
    This will make sure that episodes are no longer treated as spoilers after
    they've been watched in the current "tab session".

--- source/features/hide-spoilers.tsx
+++ source/features/hide-spoilers.tsx
@@ -2,20 +2,31 @@ import elementReady from 'element-ready'
 
 import settings from '../libs/settings'
 
-function markWatchedEpisodes() {
-  for (const episode of document.querySelectorAll('.dv-episode-container')) {
-    if (episode.querySelector('.dv-linear-progress')) {
-      episode.classList.add('rpv-watched')
+function checkEpisode(episode: Element) {
+  return episode.classList.toggle(
+    'rpv-watched',
+    !!episode.querySelector('.dv-linear-progress'),
+  )
+}
+
+function markWatchedEpisodes(episodes: NodeListOf<Element>) {
+  for (const episode of episodes) {
+    if (!checkEpisode(episode)) {
+      const observer = new MutationObserver(() => {
+        if (checkEpisode(episode)) observer.disconnect()
+      })
+      observer.observe(
+        episode.querySelector('.dv-el-status-wrapper'),
+        { childList: true, subtree: true },
+      )
     }
   }
 }
 
 export default async function() {
   const episodeList = await elementReady('#dv-episode-list > :first-child')
-
+  markWatchedEpisodes(episodeList.querySelectorAll('.dv-episode-container'))
   settings.onChange(({showSpoilers}) => {
     episodeList.dataset.rpvSpoilers = showSpoilers
   })
-
-  markWatchedEpisodes()
 }
