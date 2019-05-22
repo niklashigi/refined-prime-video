380d46d6e38204bd3574a9453a4211cb0ee6d5ee
Niklas Higi <niklas@shroudedcode.com>
2019-05-22 21:40:15 +0200

    Implement basic popup functionality

---
+++ source/libs/fetch-my-videos.ts
@@ -0,0 +1,54 @@
+const STOREFRONT_ENDPOINT_URL = 'https://www.amazon.de/gp/video/api/storefront'
+
+export default async function fetchMyVideos() {
+  const storefront = await fetchStorefront()
+
+  const collection = storefront.collections.filter(c => c.edit)[0]
+  if (!collection) throw new Error('No videos found!')
+
+  return collection.items.map(parseCollectionItem)
+}
+
+async function fetchStorefront() {
+  return (await (await fetch(STOREFRONT_ENDPOINT_URL)).json()) as Storefront
+}
+
+function parseCollectionItem(item: Item) {
+  return {
+    id: item.titleID,
+    title: item.title,
+    isPrime: item.entitlements.includes('prime'),
+    hasSubtitles: item.hasSubtitles === '1',
+    maturityRating: item.maturityRating ? parseInt(item.maturityRating.rating) : undefined,
+    image: item.image.url,
+  }
+}
+
+interface Storefront {
+  collections: Collection[]
+}
+
+interface Collection {
+  text: string
+  items: Item[]
+  edit?: any
+}
+
+interface Item {
+  link: Image
+  hasSubtitles: string
+  entitlements: 'prime'[]
+  maturityRating?: MaturityRating
+  image: Image
+  title: string
+  titleID: string
+  edit?: any
+}
+
+interface Image {
+  url: string
+}
+
+interface MaturityRating {
+  rating: string
+}
--- source/popup/popup.ts
+++ source/popup/popup.ts
@@ -1,1 +1,10 @@
-document.write('Hello from the script!')
+import fetchMyVideos from '../libs/fetch-my-videos'
+
+document.write('Loading your videos…')
+
+fetchMyVideos().then(videos => {
+  document.write('<ul>' + videos.map(video => {
+    const url = `https://www.amazon.de/gp/video/detail/${video.id}?autoplay=1`
+    return `<li><a href="${url}">${video.title}</a></li>`
+  }).join('') + '</ul>')
+})
