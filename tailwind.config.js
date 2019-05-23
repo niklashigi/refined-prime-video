2816d35d435e9a2ecbdf3a83d603af6ddc4c3b08
Niklas Higi <niklas@shroudedcode.com>
2019-05-23 20:25:12 +0200

    Use new color scheme based on Prime Video site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      white: '#fff',
+      black: '#000',
+      amazon: {
+        300: '#ffc770',
+        400: '#ff9633',
+        500: '#ff9633',
+      },
+      prime: {
+        300: '#00cefb',
+        400: '#00c1fa',
+        500: '#00aadc',
+      },
+      gray: {
+        200: '#f3f3f3',
+        300: '#e6e6e6',
+        400: '#dcdcdc',
+        500: '#959595',
+        600: '#7d7d7d',
+        700: '#767676',
+        800: '#555555',
+      },
+      carbon: {
+        600: '#24333f',
+        700: '#1e2a33',
+        800: '#1c262e',
+        900: '#141d23',
+      }
+    },
+    extend: {},
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
