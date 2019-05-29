156d90e39a51322db1edbeb5b66cdea465ec0e80
Niklas Higi <niklas@shroudedcode.com>
2019-05-29 20:03:00 +0200

    Add `showSpoilers` setting to popup settings page

---
+++ source/popup/components/show-spoilers-selector.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label
+      v-for="(option, optionId) in options"
+      :key="optionId"
+      :for="`show-spoilers-${optionId}`"
+      class="flex items-center cursor-pointer"
+    >
+      <input
+        type="radio"
+        :id="`show-spoilers-${optionId}`"
+        :checked="value === optionId"
+        @input="$emit('input', optionId)"
+        class="form-radio cursor-pointer text-xl"
+      >
+      <span class="text-lg mx-2">
+        {{ option.emoji }}
+      </span>
+      {{ option.description }}
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['value'],
+  data: () => ({
+    options: {
+      never: { emoji: '🙈', description: 'Never show spoilers' },
+      onHover: { emoji: '🔍', description: 'When hovering over an episode' },
+      always: { emoji: '🐵', description: 'Always show spoilers' },
+    },
+  }),
+}
+</script>
