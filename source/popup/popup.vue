e108bc02e04afe9f64158fa276f00c9d495f88a6
Niklas Higi <niklas@shroudedcode.com>
2019-05-29 22:17:49 +0200

    Add basic navigation to popup

--- source/popup/popup.vue
+++ source/popup/popup.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="flex flex-col h-screen bg-carbon-700 text-white">
-    <div class="z-10 bg-carbon-900 p-3 shadow">
-      What do you want to <strong>continue watching</strong>?
+    <div class="z-10 bg-carbon-900 p-3 shadow flex items-center justify-between">
+      <template v-if="screen === 'continue-watching'">
+        <div>What do you want to <strong>continue watching</strong>?</div>
+        <button
+          class="py-1 px-2 rounded bg-carbon-600 hover:bg-carbon-500 text-gray-300 hover:text-gray-200"
+          @click="screen = 'settings'"
+        >Settings</button>
+      </template>
+      <template v-else-if="screen === 'settings'">
+        <div><strong>Refined Prime Video</strong> settings</div>
+        <button
+          class="py-1 px-2 rounded bg-carbon-600 hover:bg-carbon-500 text-gray-300 hover:text-gray-200"
+          @click="screen = 'continue-watching'"
+        >Go back</button>
+      </template>
     </div>
+
     <div class="flex-grow overflow-y-auto">
-      <settings-page v-if="settingsShown"/>
-      <my-videos-list v-else/>
+      <my-videos-list v-if="screen === 'continue-watching'"/>
+      <settings-page v-else-if="screen === 'settings'"/>
     </div>
   </div>
 </template>
@@ -17,7 +31,7 @@ import SettingsPage from './components/settings-page'
 export default {
   components: { MyVideosList, SettingsPage },
   data: () => ({
-    settingsShown: true,
+    screen: 'continue-watching',
   }),
 }
 </script>
