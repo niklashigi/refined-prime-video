5096426fb77aa274f6aae6efdad0ba513fe33f2e
Niklas Higi <niklas@shroudedcode.com>
2019-05-29 19:48:33 +0200

    Add settings page

---
+++ source/popup/components/settings-page.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="p-5">
+      <div class="mb-1 font-semibold">
+        What's your preferred Amazon site?
+      </div>
+      <div class="mb-3 text-gray-400">
+        This is used to fetch your <em>Continue watching</em> list.
+      </div>
+      <site-selector v-model="settings.preferredSite"/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import SiteSelector from './site-selector'
+
+export default {
+  components: { SiteSelector },
+  data: () => ({
+    settings: {
+      preferredSite: 'de',
+    },
+  }),
+}
+</script>
