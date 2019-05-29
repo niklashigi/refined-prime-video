2dc01bf02032e853f9a56578a20eefe1d7ffdd7e
Niklas Higi <niklas@shroudedcode.com>
2019-05-29 19:48:33 +0200

    Add settings page

---
+++ source/popup/components/site-selector.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label
+      v-for="(site, siteId) in sites"
+      :key="siteId"
+      :for="`site-${siteId}`"
+      class="flex items-center cursor-pointer"
+    >
+      <input
+        type="radio"
+        :id="`site-${siteId}`"
+        :checked="value === siteId"
+        @input="$emit('input', siteId)"
+        class="form-radio cursor-pointer text-xl"
+      >
+      <span class="text-lg mx-2">
+        {{ site.flag }}
+      </span>
+      amazon.<span class="font-medium">{{ site.domainSuffix }}</span>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['value'],
+  data: () => ({
+    sites: {
+      us: { flag: '🇺🇸', domainSuffix: 'com' },
+      uk: { flag: '🇬🇧', domainSuffix: 'co.uk' },
+      de: { flag: '🇩🇪', domainSuffix: 'de' },
+    },
+  }),
+}
+</script>
--- source/popup/popup.vue
+++ source/popup/popup.vue
@@ -3,14 +3,21 @@
     <div class="z-10 bg-carbon-900 p-3 shadow">
       What do you want to <strong>continue watching</strong>?
     </div>
-    <my-videos-list class="flex-grow overflow-y-auto"/>
+    <div class="flex-grow overflow-y-auto">
+      <settings-page v-if="settingsShown"/>
+      <my-videos-list v-else/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import MyVideosList from './components/my-videos-list'
+import SettingsPage from './components/settings-page'
 
 export default {
-  components: { MyVideosList },
+  components: { MyVideosList, SettingsPage },
+  data: () => ({
+    settingsShown: true,
+  }),
 }
 </script>
