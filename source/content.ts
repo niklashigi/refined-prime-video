68ebe209226d6bf3bc1c138841a091feec63d91f
Niklas Higi <niklas@shroudedcode.com>
2019-05-31 13:21:32 +0200

    Only enable improved navigation when container element is actually found

--- source/content.ts
+++ source/content.ts
@@ -3,7 +3,9 @@ import elementReady from 'element-ready'
 import hideSpoilers from './features/hide-spoilers'
 import improveNav from './features/improve-nav'
 
-elementReady('.av-retail-m-nav-container').then(() => {
+elementReady('.av-retail-m-nav-container').then(container => {
+  if (!container) return
+
   improveNav()
   hideSpoilers()
 })
