57dc5be8578c874d8387e34ac6ae09ce08067d96
Niklas Higi <niklas@shroudedcode.com>
2019-08-26 12:41:02 +0200

    Add basic support for primevideo.com

--- source/content.ts
+++ source/content.ts
@@ -4,10 +4,21 @@ import addSkipShortcut from './features/add-skip-shortcut'
 import hideSpoilers from './features/hide-spoilers'
 import improveNav from './features/improve-nav'
 
-elementReady('.av-retail-m-nav-container').then(container => {
-  if (!container) return
-
+function bootstrap() {
   addSkipShortcut()
   improveNav()
   hideSpoilers()
-})
+
+  console.log('[RPV] Features enabled!')
+}
+
+async function main() {
+  if (location.origin.endsWith('primevideo.com')) {
+    bootstrap()
+  } else {
+    const container = await elementReady('.av-retail-m-nav-container')
+    if (container) bootstrap()
+  }
+}
+
+main()
