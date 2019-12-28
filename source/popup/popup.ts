32d8258d95f6edcbfeb7a3e19cfdaa071e6bbcc5
Niklas Higi <niklas@shroudedcode.com>
2019-12-28 21:12:08 +0100

    Make popup look decent in Firefox Mobile

--- source/popup/popup.ts
+++ source/popup/popup.ts
@@ -4,3 +4,8 @@ import Popup from './popup.vue'
 new Vue({
   render: h => h(Popup),
 }).$mount('#app')
+
+// Remove width restriction so that the popup fills the page in Firefox Mobile
+if (navigator.userAgent.startsWith('Mozilla/5.0 (Android')) {
+  document.body.style.width = 'auto'
+}
