00b13bc5d56a8b8be1eed72f6829a919e54386e3
Niklas Higi <niklas@shroudedcode.com>
2020-05-14 13:05:16 +0200

    Make "Hide spoilers" feature work again

--- source/features/hide-spoilers.ts
+++ source/features/hide-spoilers.ts
@@ -1,5 +1,3 @@
-import elementReady from 'element-ready'
-
 import settings from '../libs/settings'
 
 function checkEpisode(episode: HTMLDivElement): boolean {
@@ -14,8 +12,19 @@ function markWatchedEpisodes(episodeList: HTMLOListElement): void {
   for (const episode of episodes) checkEpisode(episode)
 }
 
+function domReady(): Promise<void> {
+  return new Promise(resolve => document.addEventListener(
+    'DOMContentLoaded', () => resolve(),
+  ))
+}
+
 export default async function(): Promise<void> {
-  const episodeList: HTMLOListElement = await elementReady('#js-node-btf ol')
+  // Amazon seems to replace the `#js-node-btf ol` element in their JavaScript
+  // code which is why `elementReady` can't be used here
+  await domReady()
+
+  const episodeList: HTMLOListElement =
+    document.querySelector('#js-node-btf ol')
   if (!episodeList) return
 
   markWatchedEpisodes(episodeList)
