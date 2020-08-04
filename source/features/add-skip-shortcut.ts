5275325e1f28dc349443affdd152f6263612013f
Niklas Higi <niklas@shroudedcode.com>
2020-08-04 21:04:14 +0200

    Add detailed logging to skip shortcut feature

--- source/features/add-skip-shortcut.ts
+++ source/features/add-skip-shortcut.ts
@@ -5,14 +5,30 @@ const BUTTON_SELECTORS = [
 ]
 
 function skip(): void {
+  const button = findSkipButton()
+
+  if (button) {
+    button.click()
+    console.log('[RPV] Skip button click simulated!', button)
+  } else {
+    console.log('[RPV] No skip button found!')
+  }
+}
+
+function findSkipButton(): HTMLElement | undefined {
   for (const selector of BUTTON_SELECTORS) {
     const button: HTMLElement = document.querySelector(selector)
-    if (button) return button.click()
+    if (button) return button
   }
 }
 
 export default function(): void {
   window.addEventListener('keydown', ({ code }) => {
-    if (code === 'KeyS') skip()
+    if (code !== 'KeyS') return
+
+    console.log('[RPV] Skip shortcut triggered!')
+    skip()
   })
+
+  console.log('[RPV] Registered skip shortcut.')
 }
