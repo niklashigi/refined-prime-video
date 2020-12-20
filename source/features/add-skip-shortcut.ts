4f6950a483e57cc4f91504f5287e68853201f5ca
Niklas Higi <niklas@shroudedcode.com>
2020-12-20 13:30:09 +0100

    Add new selectors for skip buttons
    Addresses some of the problems in #10

--- source/features/add-skip-shortcut.ts
+++ source/features/add-skip-shortcut.ts
@@ -1,7 +1,15 @@
 const BUTTON_SELECTORS = [
+  // May 2019 (still most users)
   /* Pre-roll ad */ '.adSkipButton.skippable',
   /* Recap or intro */ '.skipElement',
   /* Next episode countdown */ '.nextUpCard',
+
+  // November 2020 (slow rollout)
+  /* Recap or intro */ '.atvwebplayersdk-skipelement-button',
+  /* Next episode countdown */ '.atvwebplayersdk-nextupcard-wrapper',
+
+  // TODO: Figure out new selector for pre-roll ads
+  // See https://github.com/shroudedcode/refined-prime-video/issues/10
 ]
 
 function skip(): void {
