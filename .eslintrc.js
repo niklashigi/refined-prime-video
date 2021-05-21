44be45287548f8d4d902dc8c4cfcc4a6fbb5b4e1
Niklas Higi <niklas@shroudedcode.com>
2021-05-21 11:01:18 +0200

    Remove discontinued `prettier/@typescript-eslint` config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/essential',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   rules: {
     'no-undef': 'off',
